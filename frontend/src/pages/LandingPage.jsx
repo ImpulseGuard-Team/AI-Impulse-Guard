be67b838aba323f15f857d681fd9c4235be24419
Chaman3212 <chaman.gla_cs24@gla.ac.in>
2026-03-29 15:33:52 +0530

    commit

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+function LandingPage() {
+  return (
+    <>
+      <div className="container flex w-[1440px] h-[3989.25px] bg-[#0B1326] m-auto">
+        <Navbar></Navbar>
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
