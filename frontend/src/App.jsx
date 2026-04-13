541cb1fda8ba43713c708c91dab96fd55305d03b
Chaman3212 <chaman.gla_cs24@gla.ac.in>
2026-04-13 23:07:28 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,25 @@
 
 import LandingPage from "./pages/LandingPage"
-<<<<<<< HEAD
 import Login from "./pages/Login"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import LandingPage from "./pages/LandingPage";
+import InsightPage from "./pages/InsightPage";
 function App() {
   return (
 
  <>
    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-       <Route path="/login" element={Login}></Route>
-       <Route path="/" element={LandingPage}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>} />
+       <Route path="/login" element={<Login></Login>}></Route>
+       <Route path="/" element={<LandingPage></LandingPage>}></Route>
+       <Route path="insights" element={<InsightPage></InsightPage> }/>
       </Routes>
     </BrowserRouter>
  </>
  )
-=======
-import LoginPage from "./pages/Login"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import InsightPage from "./pages/InsightPage";
 
 
-function App() {
-  return (
-
-       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage></LandingPage>} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<LoginPage></LoginPage>} />
-        <Route path="/insights" element={<InsightPage></InsightPage>} />
-       
-      </Routes>
-    </BrowserRouter>
-  )
-
-
->>>>>>> bede381f2e82cad39e4dfe7a020c782803ff2040
 
 }
 
